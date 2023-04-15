f1c1923cbf410f19f61dcf55647f1c09dad4349b
koty <koty_166@mail.ru>
2023-04-15 17:23:57 +0300

    AddMessageClass

---
+++ QTServ/message.cpp
@@ -0,0 +1,7 @@
+#include "message.h"
+
+Message::Message(QObject *parent)
+    : QObject{parent}
+{
+
+}
---
+++ QTServ/message.h
@@ -0,0 +1,20 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include <QObject>
+#include <QDateTime>
+
+class Message : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Message(QObject *parent = nullptr);
+    QString Sender;
+    QString MessageText;
+    QDateTime MessageTime;
+
+signals:
+
+};
+
+#endif // MESSAGE_H
